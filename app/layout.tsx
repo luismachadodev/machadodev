6e24c8e1060f2cc3090b0ee0bbe1d9eefbbee177
Luis Henrique <luis@prover.net.br>
2024-07-24 13:52:39 -0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import './global.css'
 import type { Metadata } from 'next'
 import { Karla } from "next/font/google";
 import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { baseUrl } from './sitemap'
 
 import { ScrollToTop } from './components/scroll-to-top';
@@ -58,8 +56,6 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
-          <Analytics />
-          <SpeedInsights />
         </main>
         <div className="mx-auto max-w-prose"><ScrollToTop /></div>
       </body>
